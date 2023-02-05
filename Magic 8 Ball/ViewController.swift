057279eb5ee484e7a008823eefa62eea0a1e6b75
Lais Fialho <laisffn@hotmail.com>
2023-02-05 02:23:55 -0300

    Add layout and logic to the magic ball to work

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+    override func viewDidLoad() {
+        ballImageView.image = ballArray.randomElement()
+    }
+    
+    @IBAction func askButton(_ sender: Any) {
+        ballImageView.image = ballArray.randomElement()
+    }
 
 }
 
